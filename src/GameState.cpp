cabbf04a88e8e58275fd4816cf73960d150424d5
Excellente <pcheerza@gmail.com>
2017-09-26 18:02:07 +0200

    adding function
    adding function definition removed from *.hpp

--- src/GameState.cpp
+++ src/GameState.cpp
@@ -106,3 +106,34 @@ int GameState::loadWallState(Wall *w)
     }
     return (0);
 }
+
+namespace boost
+{
+    namespace serialization
+    {
+        template <class archive>
+        void serialize(archive &ar, Player &p, const unsigned int version)
+        {
+            unsigned int vers;
+
+            ar & p.yPos;
+            ar & p.xPos;
+            vers = version;
+        }
+
+        template <class archive>
+        void serialize(archive &ar, Wall &w, const unsigned int version)
+        {
+            unsigned int vers;
+
+            ar & w.xPos;
+            ar & w.yPos;
+            // ar & w.isDestructable;
+            // ar & w.VertexArrayID;
+            // ar & w.vertexbuffer;
+            // ar & w.wallTexture;
+            // ar & w.elementBuffer;
+            vers = version;
+        }
+    }
+}
